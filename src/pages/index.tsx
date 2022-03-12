00c1870dbd850937460227d6813e2cee43f0c688
Renan Pereira Loureiro <renanpl47@gmail.com>
2022-03-12 15:57:20 -0300

    feat: login page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,59 @@
-import { Flex } from "@chakra-ui/react"
+import {
+  Button,
+  Flex,
+  Input,
+  Stack,
+  FormLabel,
+  FormControl,
+} from "@chakra-ui/react"
+
 export default function Home() {
-  return <h1>Almost before we knew it</h1>
+  return (
+    <Flex w="100vw" h="100vh" align="center" justify="center">
+      <Flex
+        as="form"
+        width="100%"
+        maxWidth={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+        flexDirection="column"
+      >
+        <Stack spacing="4">
+          <FormControl>
+            <FormLabel htmlFor="email">E-mail</FormLabel>
+            <Input
+              name="email"
+              type="email"
+              focusBorderColor="pink.500"
+              variant="filled"
+              bg="gray.900"
+              _hover={{
+                bgColor: "gray.900",
+              }}
+              size="lg"
+            />
+          </FormControl>
+
+          <FormControl>
+            <FormLabel htmlFor="password">Senha</FormLabel>
+            <Input
+              name="password"
+              type="password"
+              focusBorderColor="pink.500"
+              variant="filled"
+              bg="gray.900"
+              _hover={{
+                bgColor: "gray.900",
+              }}
+              size="lg"
+            />
+          </FormControl>
+        </Stack>
+        <Button type="submit" mt="6" colorScheme="pink" size="lg">
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
+  )
 }
