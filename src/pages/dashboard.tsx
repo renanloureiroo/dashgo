aa99f8d7960311b3478f882c65018213f96b2359
Renan Pereira Loureiro <renanpl47@gmail.com>
2022-03-13 01:50:00 -0300

    feat: sidebar

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,11 +1,16 @@
+import { Flex } from "@chakra-ui/react"
 import { Header } from "../components/Header"
+import { Sidebar } from "../components/Sidebar"
 
 const Dashboard = () => {
   return (
-    <>
+    <Flex direction="column" h="100vh">
       <Header />
-      <h1>Dashboard</h1>
-    </>
+
+      <Flex width="100%" my="6" maxWidth={1480} mx="auto" px="6">
+        <Sidebar />
+      </Flex>
+    </Flex>
   )
 }
 
